351edb79d6468fcd5e973027e0508ecc99a3a704
Liarokapis Alexandros <liarokapis.v@gmail.com>
2017-05-12 20:41:52 +0300

    update

--- semester6/languages1/assigment1/ex2.cpp
+++ semester6/languages1/assigment1/ex2.cpp
@@ -1,3 +1,4 @@
+
 #include <tuple>
 #include <queue>
 #include <algorithm>
@@ -7,14 +8,100 @@
 #include <string>
 #include <iterator>
 #include <fstream>
+#include <memory>
+#include <type_traits>
+#include <cstdlib>
 
+using node_t = std::tuple<bool, std::size_t, std::size_t>;
 
-std::vector<std::string> read_input(std::string name)
+template <typename Access>
+class path_iterable 
 {
+private:
+    Access _acc;
+    node_t _cur;
+    node_t _start;
+
+public:
+    path_iterable(Access acc, node_t cur, node_t start) : _acc{acc}, _cur{cur} , _start{start} {}
+
+    class const_iterator : public std::iterator<std::forward_iterator_tag, const node_t, std::ptrdiff_t, const node_t*, const node_t&>
+
+    {
+    private:
+        const path_iterable* _parent;
+        const node_t* _data;
+        
+        const_iterator(const path_iterable* parent, const node_t* data) : _parent{parent}, _data{data} {}
+    public:
+        const_iterator() : const_iterator{nullptr, nullptr} {}
+        const_iterator(const const_iterator& o) : _parent{o._parent}, _data{o._data} {}
+
+        void swap(const_iterator& o) noexcept
+        {
+            using std::swap;
+            swap(o._data, o._data);
+            swap(o._parent, o._parent);
+        }
+
+        const_iterator& operator++ ()
+        {
+            if (*(_data) == _parent->_start)
+                _data = nullptr;
+            else
+                _data = &_parent->_acc(*_data);
+            return *this;
+        }
+
+        const_iterator operator++ (int)
+        {
+            const_iterator tmp(*this);
+            if (*(_data) == _parent->_start)
+                _data = nullptr;
+            else
+                _data = &_parent->_acc(*_data);
+            return tmp;
+        }
+
+        bool operator== (const const_iterator& rhs) const
+        {
+            return _data == rhs._data && _parent == rhs._parent;
+        }
+
+        bool operator!= (const const_iterator& rhs) const
+        {
+            return _data != rhs._data || _parent != rhs._parent;
+        }
+
+        const node_t& operator* () const
+        {
+            return *(_data);
+        }
+
+        const node_t& operator-> () const
+        {
+            return *(_data);
+        }
+    };
+
+    const_iterator begin() const { return const_iterator(this, &_cur); }
+    const_iterator end() const { return const_iterator{this, nullptr}; }
+
+};
+
+template <typename Access>
+path_iterable<Access> make_path_iterable(Access acc, node_t cur_pos, node_t start_pos) 
+{
+    return {acc, cur_pos, start_pos};
+}
+
+
+std::vector<std::string> read_input(const std::string& name)
+{
+    std::vector<std::string> space;
+
     std::ifstream in{name};
 
-    std::vector<std::string> space;
-    
     std::string line;
 
     while (std::getline(in, line))
@@ -25,81 +112,86 @@ std::vector<std::string> read_input(std::string name)
     return space;
 }
 
-template <typename Distance, typename Node>
-using path_info = std::pair<Distance, std::vector<Node>>;
 
-template <typename Distance, typename Node, typename GetNeighbors, typename GetDistance, typename Visit, typename HasVisited>
-path_info<Distance, Node> bfs_shortest_path(Distance weight_limit, Node start, Node target, Visit visit, HasVisited has_visited, GetNeighbors get_neighbors, GetDistance get_distance)
+template <typename OuterIterator, typename Pred, typename InnerIterator = typename OuterIterator::value_type::const_iterator>
+std::pair<OuterIterator, InnerIterator> find2d_if(OuterIterator begin, OuterIterator end, Pred pred) 
 {
-    std::vector<std::vector<std::pair<Node, path_info<Distance, Node>>>> to_visit_neighbors(weight_limit+1);
-
-    to_visit_neighbors[0].push_back({start, {0, {start}}});
-
-    while (std::any_of(to_visit_neighbors.begin(), to_visit_neighbors.end(), 
-                [](const std::vector<std::pair<Node, path_info<Distance, Node>>>& q) { return !q.empty(); }))
+    for (auto it = begin; it != end; ++it)
     {
-        auto& cur_visit = to_visit_neighbors[0];
-        
-        while (!cur_visit.empty())
-        {
-            auto cur_node = cur_visit.back(); cur_visit.pop_back();
+        auto it2 = std::find_if(it->cbegin(), it->cend(), pred);
 
-            if (!has_visited(cur_node.first))
-                visit(cur_node.first) = cur_node.second;
-            else
-                continue;
-
-            if (cur_node.first == target)
-                return visit(target);
-
-            for (const auto& neighbor : get_neighbors(cur_node.first))
-            {
-                if (!has_visited(neighbor))
-                {
-                    auto cur_path = visit(cur_node.first);
-                    cur_path.second.push_back(neighbor);
-                    
-                    auto distance = get_distance(cur_node.first, neighbor);
-
-                    to_visit_neighbors[distance].push_back({neighbor, {cur_path.first + distance, cur_path.second}});
-                }
-                
-            }
-        }
-
-        std::rotate(to_visit_neighbors.begin(), to_visit_neighbors.begin()+1, to_visit_neighbors.end());
+        if (it2 != it->cend())
+            return {it, it2};
     }
 
-    return visit(target);
-} 
-        
-using node_t = std::tuple<bool,std::size_t, std::size_t>;
-    
+    return {end, {}};
+}
+
+
+template <typename Node, typename GetNeighbors, typename GetDistance, typename Prev, typename Dist>
+void dijstras(Node start, Prev prev, Dist dist, GetNeighbors get_neighbors, GetDistance get_distance)
+{
+    using distance = decltype(get_distance(std::declval<Node>(), std::declval<Node>()));
+    using record = std::pair<Node, distance>;
+
+    auto cmp = [](const record& p1, const record& p2){ return !(std::get<1>(p1) < std::get<1>(p2)); };
+
+    std::priority_queue<record, std::vector<record>, decltype(cmp)> queue(cmp);
+
+    queue.emplace(start, 0);
+
+    while (!queue.empty())
+    {
+        auto cur = queue.top(); queue.pop();
+
+        const auto& cur_node = std::get<0>(cur);
+        const auto& cur_dist = std::get<1>(cur);
+
+        for (const auto& neighbor : get_neighbors(cur_node))
+        {
+            auto alt_dist = cur_dist + get_distance(cur_node, neighbor);
+
+            if (alt_dist < dist(neighbor))
+            {
+                dist(neighbor) = alt_dist;
+                prev(neighbor) = cur_node;
+                queue.emplace(neighbor, alt_dist);
+            }
+        }
+    }
+}
+
+
 int main(int argc, char* argv[])
 {
-    
+
+    if (argc == 1) 
+    {
+        std::cout << "No input file given\n";
+        return EXIT_FAILURE;
+    }
+
     auto space = read_input(argv[1]);
 
     auto height = space.size();
     auto width = space[0].size();
-    
-    node_t start_pos, end_pos;
 
-    for (std::size_t i = 0; i != space.size(); ++i)
+    auto start = find2d_if(space.cbegin(), space.cend(), [](char c){ return c == 'S'; });
+    auto end = find2d_if(space.cbegin(), space.cend(), [](char c){ return c == 'E'; });
+
+    if (start.first == space.cend() || end.first == space.cend())
     {
-        for (std::size_t j = 0; j != space[i].size(); ++j)
-        {
-            if (space[i][j] == 'S')
-            {
-                start_pos = node_t{false, i,j};
-            }
-  
-            if (space[i][j] == 'E')
-            {
-                end_pos = node_t{false, i,j};
-            }
-        }
+        std::cout << "Input does not contain start and end nodes\n";
+        return EXIT_FAILURE;
     }
+
+    auto start_y = std::distance(space.cbegin(), start.first);
+    auto start_x = std::distance(start.first->cbegin(), start.second);
+    auto start_pos = node_t{false, start_y, start_x};
+
+    auto end_y = std::distance(space.cbegin(), end.first);
+    auto end_x = std::distance(end.first->cbegin(), end.second);
+    auto end_pos = node_t{false, end_y, end_x};
 
     auto get_neighbors = [&space, height, width](const node_t& node)
     {
@@ -110,11 +202,11 @@ int main(int argc, char* argv[])
 
         std::vector<node_t> neighbors {node_t{z,i,j+1}, node_t{z,i,j-1}, node_t{z,i+1, j}, node_t{z, i-1, j}};
         neighbors.erase(std::remove_if(neighbors.begin(), neighbors.end(), [&space, width, height](const node_t& node)
-        {
-            return !(std::get<1>(node) < height && 
-                     std::get<2>(node) < width &&
-                     space[std::get<1>(node)][std::get<2>(node)] != 'X');
-        }), neighbors.end());
+                    {
+                    return !(std::get<1>(node) < height && 
+                            std::get<2>(node) < width &&
+                            space[std::get<1>(node)][std::get<2>(node)] != 'X');
+                    }), neighbors.end());
 
         if (space[i][j] == 'W') 
             neighbors.emplace_back(!z, i, j);
@@ -122,7 +214,7 @@ int main(int argc, char* argv[])
         return neighbors;
     };
 
-    auto get_distance = [&space](const node_t& node1, const node_t& node2)
+    auto get_distance = [&space](const node_t& node1, const node_t& node2) -> std::size_t
     {
         auto z1 = std::get<0>(node1);
         auto z2 = std::get<0>(node2);
@@ -132,25 +224,22 @@ int main(int argc, char* argv[])
         return 1;
     };
 
-    std::vector<path_info<std::size_t, node_t>> paths(2*height*width, 
-            {std::numeric_limits<std::size_t>::max(), {}});
-    
-    auto visit = [&paths, height, width](const node_t& node) -> path_info<std::size_t, node_t>&
-    {
-        return paths[std::get<0>(node)*(height*width) + std::get<1>(node)*width + std::get<2>(node)]; 
+    std::vector<std::size_t> dists(2 * height * width, std::numeric_limits<std::size_t>::max());
+    std::vector<node_t> prevs(2 * height * width, node_t{});
+
+    auto prev = [&prevs, height, width](const node_t& node) -> node_t&
+    { 
+        return prevs[std::get<0>(node)*(height*width) + std::get<1>(node)*width + std::get<2>(node)]; 
     };
 
-    auto has_visited = [&visit](const node_t& node)
+    auto dist = [&dists, height, width](const node_t& node) -> std::size_t&
     {
-        return visit(node).first != std::numeric_limits<std::size_t>::max();
+        return dists[std::get<0>(node)*(height*width) + std::get<1>(node)*width + std::get<2>(node)]; 
     };
 
-    auto result = bfs_shortest_path(2, start_pos, end_pos, visit, has_visited, get_neighbors, get_distance);
-    auto shortest_path = result.second;
-    auto shortest_distance = result.first;
+    dijstras(start_pos, prev, dist, get_neighbors, get_distance);
 
     std::string directions;
-    directions.reserve(shortest.path.size());
 
     auto to_direction = [](const node_t& node1, const node_t& node2)
     {
@@ -176,9 +265,13 @@ int main(int argc, char* argv[])
         }
     };
 
-    std::transform(shortest_path.begin(), std::prev(shortest_path.end()), std::next(shortest_path.begin()), std::back_inserter(directions), to_direction);
+    auto path_iterable = make_path_iterable(prev, end_pos, start_pos);
 
-    std::cout << shortest_distance << ' ';
+    std::transform(std::next(path_iterable.begin()), path_iterable.end(), path_iterable.begin(), std::back_inserter(directions), to_direction);
+
+    std::reverse(directions.begin(), directions.end());
+
+    std::cout << dist(end_pos) << ' ';
 
     for (auto d : directions)
     {
@@ -187,14 +280,6 @@ int main(int argc, char* argv[])
 
     std::cout << '\n';
 
-    return 0;
+    return EXIT_SUCCESS;
 }
-
-
-
-
-  
-
-
-        
 
--- semester6/languages1/assigment1/ex2.sml
+++ semester6/languages1/assigment1/ex2.sml
@@ -22,7 +22,7 @@ in
 end
 
 
-(* Returns the position of a node that satisfies the given predicate *)
+(* Returns the first node whose value equals the given character *)
 
 fun searchFor (height, width) vec c =
 let
@@ -41,16 +41,19 @@ end
 
 (* Used to retrieve the info regarding the best path to a given node *)
 
-fun accessInfo (height, width) vec (z,y,x) =
-  if z then Vector.sub(vec, height*width + y*width + x)
-  else Vector.sub(vec, y*width + x)
+fun accessInfo (height, width) arr (z,y,x) =
+  if z then Array.sub(arr, height*width + y*width + x)
+  else Array.sub(arr, y*width + x)
 
 
-(* Used to update the info regarding the best path of a given node *)
+(* Used to update the info regarding the best path of a given node. 
+ * This could return unit but returning the updated container makes our
+ * solveProblem function able to work with immutable data structures so it's
+ * a bit more general. *)
 
-fun update (height, width) vec (z,y,x) new =
-  if z then Vector.update(vec, height*width + y*width + x, new)
-  else Vector.update(vec, y*width + x, new)
+fun update (height, width) arr (z,y,x) new =
+  if z then (Array.update(arr, height*width + y*width + x, new); arr)
+  else (Array.update(arr, y*width + x, new); arr)
 
 
 (* Checks if we have found the best path to a given node *)
@@ -58,43 +61,66 @@ fun update (height, width) vec (z,y,x) new =
 fun visited bounds info pos = (#1 (accessInfo bounds info pos : int * (bool * int * int) list)) <> Option.valOf Int.maxInt
 
 
-(* Creates an empty vector that will contain the info regarding the best path to
- * each node *)
+(* Creates an empty array that will contain the info regarding the best path to
+ * each node. Ideally we would like to have something similar to Clojure's
+ * vector type but this should do. *)
 
-fun empty (height, width) = Vector.tabulate (2*height*width, fn _ => (Option.valOf Int.maxInt, []))
+fun empty (height, width) = Array.tabulate (2*height*width, fn _ => (Option.valOf Int.maxInt, []))
 
 
 (* This is the function that does most of the work.
- * The logic behind the function is that given all visited nodes, all nodes with best
- * distance i and some nodes with best distance (i+1), it's possible to find all nodes
- * of best distance (i+1) and some nodes of best distance (i+2). In general, this can be
- * further generalized to a O(k*n) algorithm where n is the number of nodes 
- * and k is the the max weight of the graph divided by the gcd of all weights. *)
+ *
+ * First, some definitions:
+ * - The tail of a path is the path that results if we remove the last node of 
+ *   the given path.
+ * - A 1-diff path is a path whose tail's total distance differs by 1
+ * - A 2-diff path is a path whose tail's total distance differs by 2
+ *
+ * The logic behind the function is that given:
+ * - All best paths of length i
+ * - All best 2-diff paths of length (i+1)
+ * It is possible to find:
+ * - All best paths of length (i+1)
+ * - All best 2-diff paths of length (i+2)
+ * 
+ * Proof: For a weight limit of 2, any best path is either 1-diff or
+ * 2-diff and it's tail is itself a best path.
+ * By taking all neighbors of the last node of every best path of length i,
+ * we are bound to find all 1-diff best paths of length (i+1) and all 2-diff best
+ * paths of length (i+2). We already have the 2-diff best paths of length
+ * (i+1) and the union of the 2-diff and 1-diff best paths of length (i+1) gives
+ * us all best paths of length (i+1).
+ *
+ * Note: This can be generalized to a O(k*n) algorithm where n is the number of
+ * nodes and k is the heightest weight in the graph divided by the gcd of all
+ * weights. 
+ *
+ * The pathsN function performs the above inductive step with a few small
+ * modifications: The function also has to keep track of best paths and a few filtering steps
+ * must be applied in order to remove non-best paths.
+ *)
 
 fun solveProblem neighbors visited update empty start = 
 let 
-  fun pathsN (info, pathsCur, pathsNext) = 
+  fun pathsN (info, [], []) = info
+    | pathsN (info, pathsCur, pathsNext) =  
   let
     fun neighboringPaths (node, dist, path) = map (fn (d,n) => (d, n, dist + d, n :: path)) (neighbors node)
     fun updateIfUnvisited ((node, dist, path),(info, paths)) =
       if not (visited info node) 
         then (update info node (dist, path), (node, dist, path) :: paths)
         else (info, paths)
-    val (info', newPathsCur) = foldr updateIfUnvisited (info, []) pathsCur
-    val allNeighboringPaths = List.concat (map neighboringPaths newPathsCur)
+    val (info', newPathsCur) = List.foldl updateIfUnvisited (info, []) pathsCur
+    val allNeighboringPaths = List.filter (fn (_,n,_,_) => not (visited info' n)) (List.concat (map neighboringPaths newPathsCur))
     val (neighboringPathsCost1, neighboringPathsCost2) = List.partition (fn (d,_,_,_) => d = 1) allNeighboringPaths
     fun stripCost (_,n,d,p) = (n,d,p)
-    val pathsNext' = map stripCost neighboringPathsCost2 
+    val pathsNext' = map stripCost neighboringPathsCost2
     val pathsCur' = map stripCost neighboringPathsCost1 @ pathsNext
   in
-    (info', pathsCur', pathsNext')
+    pathsN (info', pathsCur', pathsNext')
   end
-  fun repeatWhile pred f x = 
-    if pred x then repeatWhile pred f (f x) 
-    else x
-  val (result, _, _) = repeatWhile (fn (_, x, y) => not (null x andalso null y)) pathsN (empty, [(start, 0, [start])], [])
 in
-  result
+  pathsN (empty, [(start, 0, [start])], [])
 end
 
 
@@ -138,14 +164,14 @@ let
   val visited' = visited bounds
   val update' = update bounds 
   val empty' = empty bounds
-  val start' = searchFor bounds space #"S"
-  val target' = searchFor bounds space #"E"
-  val info = solveProblem neighbors' visited' update' empty' start'
-  val result = accessInfo bounds info target'
+  val start = searchFor bounds space #"S"
+  val target = searchFor bounds space #"E"
+  val info = solveProblem neighbors' visited' update' empty' start
+  val result = accessInfo bounds info target
   val dist = #1 result
   val directions = toDirections (rev (#2 result))
 in
   print ((Int.toString dist) ^ " " ^ directions ^ "\n")
 end;
 
-main ();
+main (); 
